0f3eb69ed8b5c9663d0faa2c91b29815103195d9
starkhorn <t.earendel@gmail.com>
2016-11-23 21:23:30 +0700

    Fix airbnb style. Add fabric.js

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -39,6 +39,9 @@ module.exports = {
       'js': 'never',
       'vue': 'never'
     }],
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
     {{/if_eq}}
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
